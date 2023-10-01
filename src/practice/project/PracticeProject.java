173cbdefd25a874b9c2a9863e3ae7c5b88d6c2ff
AMMAD-2022532 <146199375+AMMAD-2022532@users.noreply.github.com>
2023-10-01 22:48:54 +0100

    Update PracticeProject.java
    my full name

--- src/practice/project/PracticeProject.java
+++ src/practice/project/PracticeProject.java
@@ -15,6 +15,8 @@ public class PracticeProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello world");
+        System.out.println("My name is Ammad hussain");
     }
     
 }
